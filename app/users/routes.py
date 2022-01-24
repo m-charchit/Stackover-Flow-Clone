8f28263eb393c3f1db714806284f34f315621e8e
Mrinmoy Haloi <contact@mrinmoy.tech>
2022-01-24 12:52:27 +0530

    organised users routes code

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,64 +1,55 @@
-from flask import Blueprint,render_template,session,request,redirect,flash,abort
-from flask_sqlalchemy import SQLAlchemy
-# from flask_login import login_user, current_user, logout_user, login_required
+from flask import (
+    Blueprint,
+    render_template,
+    session,
+    request,
+    redirect,
+    flash,
+    abort
+)
 from app import db
-# from app.users.forms import RegistrationForm,LoginForm,Account,ChangeEmail,Confirm_email,ResetPassword
 from app.models import *
-# from app.users.utils import send_mail,save_picture
-# from random import randint
-# from datetime import datetime
 from app.users.utils import *
-from sqlalchemy import text,desc,func
+from sqlalchemy import func
 
-users = Blueprint('users', __name__)         
+users = Blueprint('users', __name__)
 
 
-
-# profile page of a person. still working
+# profile page of a person.
 @users.route("/profile/<string:name>")
 def profile(name):
-    
     user = Detail.query.filter_by(username=name).first()
     return render_template("profile.html",
-                           user=user
-                           )
-# page to edit profile details. still working on it
-@users.route("/profile/edit/<string:name>",methods=["POST","GET"])
+                           user=user)
+
+
+# page to edit profile details.
+@users.route("/profile/edit/<string:name>", methods=["POST", "GET"])
 def edit_profile(name):
 
     if "user" in session:
         if session["user"] == name:
             user = Detail.query.filter_by(username=session["user"]).first()
             if request.method == "POST":
-                # person = Detail.query.filter_by(username=request.form["username"]).first()
                 user.title = request.form["title"]
                 user.about = request.form["about"]
                 user.website_link = request.form["website"]
                 user.twitter = request.form["twitter"]
                 user.linkedin = request.form["linkedin"]
                 user.github_username = request.form["github"]
-                # taking the image 
+                # taking the image
                 try:
                     user.pic = upload_images(request.files["pic"])
                 except:
                     pass
-
-
-                # if not person or person.username == session["user"] :
-                #     user.username = request.form["username"]
-                    
-                #     session["user"] = user.username
-                #     flash("Details Updated Successfully","success")
-                # else:
-                #     flash("username already taken","danger")
                 db.session.commit()
-            return render_template("profile_edit.html",user=user)
-            
+            return render_template("profile_edit.html", user=user)
+
         abort(404)
     abort(404)
 
 
-#signup system
+# signup system
 @users.route('/signup', methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
@@ -66,51 +57,46 @@ def signup():
         email = request.form.get("email")
         password = request.form.get("password")
         cpassword = request.form.get("cpassword")
-        contact = db.session.query(Detail).filter(func.lower(Detail.username) == name.lower()).first()
-        if password == cpassword and contact == None:
+        contact = db.session.query(Detail).filter(func.lower(
+            Detail.username) == name.lower()).first()
+        if password == cpassword and contact is None:
             entry = Detail(username=name,
                            email=email,
                            password=password,
-                           date=datetime.now())
+                           date=datetime.strftime(datetime.now(), "%d-%m-%y"))
             db.session.add(entry)
             db.session.commit()
             return redirect("/signin")
-        elif contact != None:
+        elif contact is not None:
             flash("Username already taken ", "danger")
         elif password != cpassword:
-            flash("The passwords does not match", "danger")
+            flash("Confirmation passwords does not match", "danger")
         else:
-            flash(
-                "Sorry , You were not able to SignUp. please contact the creator"
-            )
+            flash("Sorry , we couldn't create your account. Please try again", "danger")
     return render_template("signup.html")
 
-#signin system
+
+# signin system
 @users.route('/signin', methods=["GET", "POST"])
 def signin():
-    if "user" in session and session["loggedin"] == True:
+    if "user" in session and session["loggedin"] is True:
         return redirect("/")
 
     elif request.method == "POST":
 
         username = request.form.get("username").lower()
         password = request.form.get("password")
-        contact = db.session.query(Detail).filter(func.lower(Detail.username) == username).first()
+        contact = db.session.query(Detail).filter(func.lower(
+            Detail.username) == username).first()
 
         if contact:
 
-            if username == contact.username.lower() and password == contact.password:
+            if username == contact.username.lower() and \
+                    password == contact.password:
 
                 session["user"] = contact.username
-                
+
                 session["loggedin"] = True
-                # session["random_otp"] = randint(23145,91875)
-                # mail.send_message(f"ToDo List App OTP",
-                # sender=contact.email,
-                # recipients=[contact.email],
-                # body =  "Please use this otp for login in to your account\n" + str(session["random_otp"])
-                # )
-                # return redirect("/otp")
                 return redirect("/")
 
             else:
@@ -118,9 +104,8 @@ def signin():
                 return render_template("signin.html")
 
         else:
-            flash(
-                "No user exist of name " + username +
-                " Please Sign Up to continue   ", "danger")
+            flash(f"No user exist of name {username}.\
+                 Please sign up to continue", "danger")
             return render_template("signin.html")
     else:
         return render_template("signin.html")
